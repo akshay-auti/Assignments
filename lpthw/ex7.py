80b8981bed4eb1b01e4f01955a269be8ede84327
akshay-auti <akshay.auti@neosofttech.com>
2017-10-06 17:55:34 +0530

    Py_ex_6to10

---
+++ lpthw/ex7.py
@@ -0,0 +1,21 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s" % 'snow'
+print "and everywhere that mary went."
+print "." * 10 # 10 times '.' printed
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, # if we remove comma cursor goes to newline
+print end7 + end8 + end9 + end10 + end11 + end12
