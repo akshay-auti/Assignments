f1e726f5e6e3d5df65936bbc00a7888b2e85a542
akshay-auti <akshay.auti@neosofttech.com>
2017-10-06 14:56:28 +0530

    Py_Ex_1to5

---
+++ lpthw/ex5.py
@@ -0,0 +1,18 @@
+my_name = "Zed_shaw"
+my_age = 35
+my_height = 175
+my_weight = 80
+my_eyes = "blue"
+my_teeth = "White"
+my_hair = "brown"
+
+print "Let's talk about %s" % my_name
+print "He is %d year old" % my_age
+print "He is %d cm tall" % my_height
+print "He is %d pounds heavy" % my_weight
+print "He got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s" % my_teeth
+
+# this is different line
+print "If I add %d, %d and %d  I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
